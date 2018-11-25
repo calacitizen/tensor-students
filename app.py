a4e4475d75f71256d0e75d93c37071d9d176dd37
nr.izygin <nr.izygin@tensor.ru>
2018-11-26 00:04:50 +0300

    shit

--- app.py
+++ app.py
@@ -2,12 +2,15 @@
 # Imports
 #----------------------------------------------------------------------------#
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 # from flask.ext.sqlalchemy import SQLAlchemy
 import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
+import chess
+import chess.uci
+import operator
 
 #----------------------------------------------------------------------------#
 # App Config.
@@ -44,6 +47,38 @@ def login_required(test):
 @app.route('/')
 def home():
     return render_template('pages/placeholder.home.html')
+
+@app.route('/game')
+def game():
+    return render_template('pages/game.html')
+
+@app.route('/game/ask', methods=['POST'])
+def api_ask():
+    game = request.get_json(force=True)
+    if  game['game_set']:
+        board = chess.Board(game['game_set'])
+        handler = chess.uci.InfoHandler()
+        engine = chess.uci.popen_engine('/Users/mraz/Projects/stockfish-9-mac/Mac/stockfish-9-64')
+        engine.info_handlers.append(handler)
+        engine.position(board)
+        moves = {}
+        moves['turn'] = board.turn
+        moves['variants'] = []
+        for el in board.legal_moves:
+            engine.go(searchmoves=[el],movetime=1000)
+            if (handler.info["score"][1].cp is None or isinstance(handler.info["score"][1].cp, float) ):
+                if handler.info["score"][1].mate == 1:
+                    moves['variants'].append({"move": board.san(el), "score": 9999, "mate": handler.info["score"][1].mate})
+                elif handler.info["score"][1].mate == -1:
+                    moves['variants'].append({"move": board.san(el), "score": -9999, "mate": handler.info["score"][1].mate})
+                else:
+                    moves['variants'].append({"move": board.san(el), "score": -9999, "mate": handler.info["score"][1].mate})
+            else:
+                moves['variants'].append({"move": board.san(el), "score": round(handler.info["score"][1].cp/100.0,2), "mate": handler.info["score"][1].mate}) 
+            moves['variants'].sort(key=operator.itemgetter('score'), reverse=True)
+        return jsonify(moves)
+    else:
+        return jsonify({'error': 'no params entered'})
 
 
 @app.route('/about')
