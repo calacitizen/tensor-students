91fcc4c6d21b0154c19fa252a7ba6de7f6f4de2d
nr.izygin <nr.izygin@tensor.ru>
2018-11-26 00:44:31 +0300

    app

--- app.py
+++ app.py
@@ -131,12 +131,11 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     app.run()
+'''
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+	port = int(os.environ.get('PORT', 8000))
+	app.run(host='0.0.0.0', port=port,debug=True)
